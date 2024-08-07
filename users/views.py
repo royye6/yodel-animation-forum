4f161704b5132cc215651937685a16ebda686b78
royye6 <royye6@gmail.com>
2024-08-07 15:41:47 +0200

    add user logout

--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login
 from .forms import RegisterForm, LoginForm
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, auth
 
 
 def user_register(request):
@@ -13,7 +14,7 @@ def user_register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('login')
+            return redirect('/')
         else:
             messages.info(request, 'Please fill in all the required fields')
             return render(request, 'users/templates/users/register.html', context)
@@ -21,17 +22,24 @@ def user_register(request):
         return render(request, 'users/templates/users/register.html', context)
 
 
-
 def user_login(request):
     context = {'form': LoginForm}
-
+    # signed_in = False
     if request.method == 'POST':
         form = LoginForm(data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
-            return redirect('/')
+            if login:
+                # context['signed_in'] = signed_in
+                return redirect('/') 
         else:
             messages.info(request, 'Invalid Credentials')
             return render(request, 'users/templates/users/login.html', context)
     else:
         return render(request, 'users/templates/users/login.html', context)
+    
+
+@login_required
+def user_logout(request):
+    auth.logout(request)
+    return redirect('/')
